06dd04286a1544fb9653343c1477ee5ddc130d3b
Elias Ibañez <elias-ibanez@outlook.es>
2025-01-15 14:45:43 -0500

    implementacion de socket.io

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,9 @@ import { Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers  } = useAuthStore();
+
+  console.log({ onlineUsers });
 
   useEffect(() => {
     checkAuth();
