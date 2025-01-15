cf57fae364c8b1269bf2d91b04cf3220805a5ff6
Elias Ibañez <elias-ibanez@outlook.es>
2025-01-15 01:16:35 -0500

    Creacion de modelos y rutas para la gestion de los mensajes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -88,5 +88,29 @@ export const logout = (req, res) => {
 }
 
 export const updateProfile = async(req, res) => {
+    try {
+        const { profilePic } = req.body;
+        const userId = req.user._id;
 
+        if(!profilePic) {
+            return res.status(400).json({message: "Profile pic is required" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        console.log("Error in update profile");
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller");
+        res.status(500).json({message: "Internal server error"})
+    }
 }
