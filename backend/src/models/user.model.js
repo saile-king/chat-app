a7be48348980e63bdaa21452c516c0f1fc36a0c2
Elias Ibañez <elias-ibanez@outlook.es>
2025-01-14 23:26:14 -0500

    Creacion de las rutas y metodos para el login, signup y logout de usuarios

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profile: {
+      type: Number,
+      default: 0,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
